9e5bdb3070af94eccb0ee5b79af545220727bb55
Andrew Parra <nixonandrewparra@gmail.com>
2024-04-04 14:47:20 -0500

    solucionando

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  const data = [
+    "GitHub",
+    "Frontend Mentor",
+    "LinkedIn",
+    "Twitter",
+    "Instagram",
+  ];
+  return (
+    <>
+      <div className="container-target">
+        <div className="conatiner-img">
+          <img
+            src="/assets/images/avatar-jessica.jpeg"
+            alt="Avatar img the jessica Randa"
+          />
+        </div>
+        <h2 className="title-name">Jessica Randall</h2>{" "}
+        <span className="country">London, United Kingdom </span>
+        <p className="description">
+          {'"Front-end developer and avid reader."'}
+        </p>
+        <div className="buttoms-container">
+          {data.map((data) => {
+            return <button key={data}>{data}</button>;
+          })}
+        </div>
+      </div>
+      <footer>
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Nixon Parra</a>.
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
